1e743309f6a717a84c45a6c3af2b9b7f5f910d15
Egor Andreev <eandreev@vsevcredit.ru>
2016-08-10 18:37:50 +0300

    metrics enabled next step: generate collectd.conf from etcd

--- fleetctl-init.sh
+++ fleetctl-init.sh
@@ -1,3 +1,8 @@
 fleetctl submit `dirname $0`/unit-templates/*.service
-fleetctl start apache@1 apache@2 apache-discovery@1 apache-discovery@2 nginx.service
+fleetctl start \
+    apache@{1..2} \
+    apache-discovery@{1..2} \
+    nginx.service \
+    graphite.service \
+    graphite-discovery.service
 
