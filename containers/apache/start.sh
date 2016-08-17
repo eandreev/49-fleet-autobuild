adf3e7a212bbcf33cbabe6278d83e94820501004
Egor Andreev <eandreev@vsevcredit.ru>
2016-08-17 15:50:22 +0300

    changed collectd hostname on apache

--- containers/apache/start.sh
+++ containers/apache/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i '/Hostname/c Hostname '${COREOS_PRIVATE_IPV4} /etc/collectd/collectd.conf
+sed -i '/Hostname/c Hostname '${COLLECTD_HOSTNAME} /etc/collectd/collectd.conf
 
 supervisord -n
